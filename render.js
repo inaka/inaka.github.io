28a5fd3f0dbab552a005b92996fa854b0225420a
WEREMSOFT <pablo.weremczuk@gmail.com>
2019-01-02 15:11:43 +0100

    PR fixes + fix our history section

--- render.js
+++ render.js
@@ -9,7 +9,7 @@ const navigationTemplate = {navigation: fs.readFileSync('templates/navigation.te
 const headerSectionTemplate = {headerSection: fs.readFileSync('templates/header-section.template.html').toString()};
 const whoWeAreSectionTemplate = {whoWeAreSection: fs.readFileSync('templates/who-we-are-section.template.html').toString()};
 const ourHistorySectionTemplate = {ourHistorySection: fs.readFileSync('templates/our-history-section.template.html').toString()};
-const contactUsSectionTemplate = {ourHistorySection: fs.readFileSync('templates/contact-us-section.template.html').toString()};
+const contactUsSectionTemplate = {contactUsSection: fs.readFileSync('templates/contact-us-section.template.html').toString()};
 const eventsSectionTemplate = {eventsSection: fs.readFileSync('templates/events-section.template.html').toString()};
 const projectsSectionTemplate = {projectsSection: fs.readFileSync('templates/projects-section.template.html').toString()};
 
